10033faafd70fdf6d3f0aec77754e9fdb8620ee9
JaminROCK <KingJamin@hotmail.com>
2018-02-07 18:50:32 -0600

    Update CIS2013_Week04_Quiz1.cpp

--- CIS2013_Week04_Quiz1.cpp
+++ CIS2013_Week04_Quiz1.cpp
@@ -4,18 +4,26 @@ using namespace std;
 
 int main()
 {
+    int fooCounter = 0;
+    int barCounter = 0;
+    
+    
     for(int i = 1; i <= 2000; i++)
     {
+
+        
         cout << i;
         
         if(i%50 == 0)
         {
             cout << " FOO";
+            fooCounter++;
         }
         
         if(i%33 == 0)
         {
             cout << " BAR";
+            barCounter++;
         }
         
         cout << "\n";
@@ -23,9 +31,9 @@ int main()
     }
     
     
+    cout << "The total number of times \"FOO\" was printed: " << fooCounter << endl;
+    cout << "The total number of times \"BAR\" was printed: " << barCounter << endl;
+    
     
     return 0;
 }
-
-
-
